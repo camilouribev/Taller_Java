b3a83f29cd0fe2814bcc340f4cf465945f433462
Camilo U <cauribeva@unal.edu.co>
2021-09-08 08:02:22 -0500

    Agregados punto 12 y función de chequeo de strings

--- src/com/taller_java/Main.java
+++ src/com/taller_java/Main.java
@@ -14,7 +14,8 @@ public class Main {
                     showInputDialog(null, "Que punto quieres probar?: \n 1.Comparación " +
                             "I\n 2.Comparación II\n 3. Área de un círculo \n 4. Cálculo precio con IVA \n 5. Pares While " +
                             "\n 6. Pares ciclo For "+ "\n 7. Evita negativos " + "\n 8. ¿Laboral o festivo? " +
-                            "\n 9. Concatenación de cadenas " + "\n 10. Eliminación de espacios en cadenas " + "\n 11. Contador de caracteres y vocales ", "Taller Java Sofka U", JOptionPane.QUESTION_MESSAGE);
+                            "\n 9. Concatenación de cadenas " + "\n 10. Eliminación de espacios en cadenas " +
+                            "\n 11. Contador de caracteres y vocales " +  "\n 12. Comparador de cadenas ", "Taller Java Sofka U", JOptionPane.QUESTION_MESSAGE);
             if (!esNum(inicio)) return;
             eleccion = Integer.parseInt(inicio);
 
@@ -58,6 +59,9 @@ public class Main {
 
                 case 11:
                     punto11();
+                    break;
+                case 12:
+                    punto12();
                     break;
                 default:
                     JOptionPane.showMessageDialog(null, "Ingresaste una opcion invalida",
@@ -235,6 +239,8 @@ public class Main {
         String input = JOptionPane.
                 showInputDialog(null, "Ingresa una frase: ", "Contador de caracteres y vocales", JOptionPane.QUESTION_MESSAGE);
         int size = input.length();
+        if (!checkString(input)) return;
+
         String inputLower = input.toLowerCase(Locale.ROOT);
         int vowels = 0;
             for (int i = 0; i < inputLower.length(); i++)
@@ -249,9 +255,30 @@ public class Main {
             }
 
         JOptionPane.showMessageDialog(null, "La frase ingresada tiene "+size+" caracteres y "+vowels+" vocales",
-                "Eliminación de espacios", JOptionPane.INFORMATION_MESSAGE);
+                "Contador de caracteres y vocales", JOptionPane.INFORMATION_MESSAGE);
 
     }
+
+    private static void punto12(){
+        String string_1 = JOptionPane.
+                showInputDialog(null, "Ingresa la primera palabra o frase: ", "Comparación de cadenas", JOptionPane.QUESTION_MESSAGE);
+        if (!checkString(string_1)) return;
+
+        String string_2 = JOptionPane.
+                showInputDialog(null, "Ingresa la segunda palabra o frase: ", "Comparación de cadenas", JOptionPane.QUESTION_MESSAGE);
+        if (!checkString(string_2)) return;
+        if(string_1.equals(string_2)){
+            JOptionPane.showMessageDialog(null, "Las palabras o frases son iguales",
+                    "Comparación de cadenas", JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(null, "La diferencia entre las cadenas es:\n"+diferencia(string_1,string_2),
+                    "Comparación de cadenas", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+
+
+
 
     private static boolean esNum(String input) {
         if (esDouble(input) || esInt(input)) { //inicialmente chequea si es double o int, si lo es, no hay problema y retorna falso
@@ -280,6 +307,52 @@ public class Main {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    private static boolean checkString(String input){
+        if(input.length() == 0 ){
+            JOptionPane.showMessageDialog(null, "No ingresaste ninguna frase!",
+                    "Error!", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+    public static String diferencia(String str1, String str2) {
+
+
+        if (str1 == null) {
+            return str2;
+        }
+        if (str2 == null) {
+            return str1;
+        }
+        int at = indiceDeDiferencia(str1, str2);
+        if (at == 404) {
+            return "";
+        }
+        return str2.substring(at);
+    }
+
+    public static int indiceDeDiferencia(CharSequence cs1, CharSequence cs2) {
+        if (cs1 == cs2) {
+            return 404;
+        }
+        if (cs1 == null || cs2 == null) {
+            return 0;
+        }
+        int i;
+        for (i = 0; i < cs1.length() && i < cs2.length(); ++i) {
+            if (cs1.charAt(i) != cs2.charAt(i)) {
+                break;
+            }
+        }
+        if (i < cs2.length() || i < cs1.length()) {
+            return i;
+        }
+        return 404;
     }
 
 }
